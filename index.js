9ea208636dcfd25b04857d1b17da75e41f187867
vileda <vileda@piratenfraktion-nrw.de>
2013-09-22 20:28:52 +0200

    hide button on readonly pads

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var eejs = require('ep_etherpad-lite/node/eejs');
 
 exports.eejsBlock_editbarMenuLeft = function(hook_name, args, cb) {
-  args.content += eejs.require('ep_eplmgmt/templates/eplmgmt_button.ejs');
+  if(!args.renderContext.req.url.match(/^\/(p\/r\..{16})/)) {
+    args.content += eejs.require('ep_eplmgmt/templates/eplmgmt_button.ejs');
+  }
   cb();
 };
